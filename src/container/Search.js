1ca5cdaf0de8b89f36ed1ee84af60c70d30cee59
Aleezah1429 <64039135+Aleezah1429@users.noreply.github.com>
2021-02-06 23:49:04 +0500

    Add files via upload

---
+++ src/container/Search.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { View, Text,Button,FlatList,StyleSheet,SafeAreaView} from 'react-native';
+import {SearchBar,Icon} from "react-native-elements";
+import { useState,useEffect } from 'react';
+import database from "@react-native-firebase/database";
+
+function Search(){
+const [Data,setData]=useState([]);
+global.DATA=[]
+  database().ref('/users').once("value").then(snapshot=>{
+      var result = snapshot.val();
+      // console.log(snapshot.val().json())
+      // console.log(global.result)
+      var z=Object.entries(result)
+      for(var i=0;i<z.length;i++){
+        global.DATA.push(z[i][1].user)
+      // console.log("i want",z[i][1].user)
+    } 
+  })
+
+  return global.DATA.map((element) => {
+    return (
+
+      <View  key={Math.random() * (100 - 1) + 1} style={{margin: 10,flex:1,backgroundColor:"red"}}>
+        {console.log(global.DATA)}
+        <Text style={{color:"black",fontSize:10}}>{element}</Text>
+      </View>
+    );
+  });
+}
+ 
+export default Search;
